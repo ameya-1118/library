532305386201609a2ff378044ce0a0215ba2744a
ameya <ameya1551@gmail.com>
2026-06-13 16:36:46 +0530

    Add API URL debug

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -3,6 +3,7 @@ import axios from "axios";
 // const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 const API_BASE_URL ="https://library-flc1.onrender.com/api/v1";
+console.log("API_BASE_URL =", API_BASE_URL);
 const api = axios.create({
   baseURL: API_BASE_URL,
 });
